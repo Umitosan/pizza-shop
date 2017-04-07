d566410f01123dc14e422804c47f7bc9957ec6de
Dominic Brown <yeahdom@gmail.com>
2017-04-07 11:24:37 -0700

    add functionalilty to allow up to 4 toppings from user and display them correctly

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,12 @@ function Pizza() {
 }
 
 Pizza.prototype.addTopping = function(tmpTopp) {
-  this.toppingsArr.push(tmpTopp);
-  return ;
+  if (tmpTopp !== "none") {
+    this.toppingsArr.push(tmpTopp);
+  } else {
+    console.log("wth?");
+  }
+  // return this.toppingsArr;
 }
 
 Pizza.prototype.pizzaCost = function() {
@@ -26,27 +30,34 @@ Pizza.prototype.pizzaCost = function() {
 // FRONT END
 $(document).ready(function() {
 
-
   $("#pizza-form").submit(function(event) {
     event.preventDefault();
 
     var userPizza = new Pizza();
 
     userPizza.addTopping($("#pizza-topping1 option:selected").val());
+    userPizza.addTopping($("#pizza-topping2 option:selected").val());
+    userPizza.addTopping($("#pizza-topping3 option:selected").val());
+    userPizza.addTopping($("#pizza-topping4 option:selected").val());
     userPizza.size = $("#pizza-size option:selected").val();
 
     // console.log();
 
     // display pizza summary
-    $("#pizza-summary-topping1").text("");
-    $("#pizza-summary-topping1").append(userPizza.toppingsArr);
     $("#pizza-summary-size").text("");
-    $("#pizza-summary-size").append(userPizza.size);
+    $("#pizza-summary-size").text(userPizza.size);
+    $("#pizza-summary-toppings").text("");
+    $("#pizza-summary-toppings").append(userPizza.toppingsArr.toString());
     $("#pizza-summary-area").show();
 
     // reset input dropdowns
     $("#pizza-topping1").prop('selectedIndex', 0);
+    $("#pizza-topping2").prop('selectedIndex', 0);
+    $("#pizza-topping3").prop('selectedIndex', 0);
+    $("#pizza-topping4").prop('selectedIndex', 0);
     $("#pizza-size").prop('selectedIndex', 0);
   });
+
+  
 
 });
